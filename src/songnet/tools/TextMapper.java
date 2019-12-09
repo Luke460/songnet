cecc855766278058832d45ae2ab33bb2a096be98
Luca Furlano <furlanoluca@libero.it>
2019-12-09 12:50:20 +0100

    mapping ottimization: less than 1KB for song needed

--- src/songnet/tools/TextMapper.java
+++ src/songnet/tools/TextMapper.java
@@ -20,37 +20,33 @@ public class TextMapper {
 		str.replaceAll("[^a-zA-Z0-9]", " "); 
 		
 		List<String> words = Arrays.asList(str.split(" "));
-		HashMap<String,Word> stringToWord = new HashMap<>();
 		HashMap<String,Integer> stringToNumber = new HashMap<>();
 		
 		for(String temp:words) {
 			if(temp.length()>=MINIMAL_WORD_LENGTH) {				
-				if(stringToWord.containsKey(temp)) {
-					Word existingWord = stringToWord.get(temp);
+				if(stringToNumber.containsKey(temp)) {
+					Word existingWord = new Word(temp, stringToNumber.get(temp));
 					existingWord.setOccurences(existingWord.getOccurences()+1);
 					stringToNumber.put(temp, existingWord.getOccurences());
-					stringToWord.put(temp, existingWord);
 				} else { 
-					Word word = new Word(temp, 1);
-					stringToWord.put(temp, word);
 					stringToNumber.put(temp, 1);
 				}
 			}
 		}
 		
-		stringToWord = resizeMap(stringToNumber, stringToWord);
+		stringToNumber = resizeMap(stringToNumber);
 		
-		return new DecodedSong(inputSong.getName(), inputSong.getAuthorName(), stringToWord);
+		return new DecodedSong(inputSong.getName(), inputSong.getAuthorName(), stringToNumber);
 
 	}
 
-	private static HashMap<String, Word> resizeMap(HashMap<String, Integer> stringToNumber, HashMap<String, Word> stringToWord) {
+	private static HashMap<String, Integer> resizeMap(HashMap<String, Integer> stringToNumber) {
 		HashMap<String, Integer> sortedMap = new HashMap<>();
-		HashMap<String, Word> result = new HashMap<>();
+		HashMap<String, Integer> result = new HashMap<>();
 		sortedMap = (HashMap<String, Integer>) MapUtil.sortByValue(stringToNumber);
 
 		for(String temp:sortedMap.keySet()) {
-			result.put(temp, stringToWord.get(temp));
+			result.put(temp, stringToNumber.get(temp));
 			if (result.size()>TEXT_SAMPLES_NUMBER) return result;
 		}
 		return result;
